930f07d689898795e43580b802dede1c9ba96c1a
kojokwakye <kojokwakye808@gmail.com>
2025-04-17 22:56:26 +0000

    start over

--- js.js
+++ js.js
@@ -1,27 +1,17 @@
+const myLibrary = [];
 
-  
-//   const theHobbit = new Book(
-//     "the hobbit",
-//     "j.r.r tolkien",
-//     "295",
-//     "not read yet"
-//   );
-//   console.log(theHobbit.info());
+function Book() {
+  // the constructor...
 
-
-const myLibrary = []
-
-function Book(title, author,pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-    this.info = function () {
-      return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-    };
-  
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+  };
 }
 
-function addBookTolibrary() {
-
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
 }
