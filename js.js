f482daa0fbc9099604cc4ee9a301c0fc9f55c6dc
kojokwakye <kojokwakye808@gmail.com>
2025-04-18 22:24:54 +0000

    made a prompt that ask for book name to add to lib

--- js.js
+++ js.js
@@ -1,17 +1,20 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
-
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-  };
-}
+// function Book() {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.readStatus = readStatus;
+//   this.info = function () {
+//     return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+//   };
+// }
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
+  let book = prompt("name of the book", "enter title");
+  if (book != null) {
+    document.getElementById("demo").innerHTML =
+      "you added " + book + " to your library";
+  }
 }
