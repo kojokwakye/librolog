5b89d55b30df72119403ed9b15fc3f67393abaf9
kojokwakye <kojokwakye808@gmail.com>
2025-04-20 23:58:11 +0000

    ..

--- js.js
+++ js.js
@@ -1,19 +1,28 @@
 const myLibrary = [];
 
-// function Book() {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.readStatus = readStatus;
-//   this.info = function () {
-//     return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-//   };
-// }
+// myLibrary.push(book)
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+for (let i = o; i < myLibrary.length; i++) {
   let book = prompt("name of the book", "enter title");
   if (book != null) {
     document.getElementById("demo").innerHTML = book;
   }
+}
+
+function Book() {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+  };
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  // let book = prompt("name of the book", "enter title");
+  // if (book != null) {
+  //   document.getElementById("demo").innerHTML = book;
+  // }
 }
