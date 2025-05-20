2863707c6188a8608783a5b940e0c04fa037814e
kojokwakye <kojokwakye808@gmail.com>
2025-05-20 17:41:07 +0000

    remove js for now

--- js.js
+++ js.js
@@ -1,27 +1,27 @@
-const myLibrary = [];
-// myLibrary.push(book)
+// const myLibrary = [];git
+// // myLibrary.push(book)
 
-// for (let i = o; i < myLibrary.length; i++) {
-//   let book = prompt("name of the book", "enter title");
-//   if (book != null) {
-//     document.getElementById("demo").innerHTML = book;
-//   }
+// // for (let i = o; i < myLibrary.length; i++) {
+// //   let book = prompt("name of the book", "enter title");
+// //   if (book != null) {
+// //     document.getElementById("demo").innerHTML = book;
+// //   }
+// // }
+
+// // function Book() {
+// //   this.title = title;
+// //   this.author = author;
+// //   this.pages = pages;
+// //   this.readStatus = readStatus;
+// //   this.info = function () {
+// //     return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+// //   };
 // }
 
-// function Book() {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.readStatus = readStatus;
-//   this.info = function () {
-//     return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-//   };
-}
-
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-  // let book = prompt("name of the book", "enter title");
-  // if (book != null) {
-  //   document.getElementById("demo").innerHTML = book;
-  // }
-}
+// function addBookToLibrary() {
+//   // take params, create a book then store it in the array
+//   // let book = prompt("name of the book", "enter title");
+//   // if (book != null) {
+//   //   document.getElementById("demo").innerHTML = book;
+//   // }
+// }
