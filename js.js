6268e9f4238850aec46020b1408c14939bf816a9
kojokwakye <kojokwakye808@gmail.com>
2025-06-05 23:35:19 +0000

    nnn

--- js.js
+++ js.js
@@ -10,3 +10,14 @@
       bookInput.setCustomValidity('');
     });
   });
+
+  
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
