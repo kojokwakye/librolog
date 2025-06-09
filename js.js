6028ca045ac97ee54b51c240330c495fcdad1913
kojokwakye <kojokwakye808@gmail.com>
2025-06-09 20:20:20 +0000

    functioning homepage

---
+++ js.js
@@ -0,0 +1,10 @@
+function formPopUp() {
+  document.getElementById("form").style.display = "block";
+  document.getElementById('log').style.display = 'none'
+}
+
+function closeForm() {
+  document.getElementById("form").style.display = "none";
+  document.getElementById('log').style.display = 'block'
+
+}
