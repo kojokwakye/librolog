e47ff12dc24f829d36a68bbd63bbe73b330c03bb
kojokwakye <kojokwakye808@gmail.com>
2025-06-16 21:36:05 +0000

    ???

--- js.js
+++ js.js
@@ -1,12 +1,9 @@
+// const mainpage = document.querySelector("mainpage");
+// const content = document.querySelector(".content");
 const dialog = document.querySelector("dialog");
 const logButton = document.querySelector("dialog  + button");
 const closButton = document.querySelector("dialog button");
 const submitBtn = bookDialog.querySelector("#submit");
-
-// // form
-// let title = document.getElementById("title");
-// let author = document.getElementById("author");
-// let pages = document.getElementById("pages");
 
 logButton.addEventListener("click", () => {
   dialog.showModal();
@@ -18,38 +15,39 @@ closButton.addEventListener("click", () => {
 
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book() {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
+  this.title = title.value;
+  this.author = author.value;
+  this.pages = pages.value;
   this.id = crypto.randomUUID();
-  // addBookToLibrary();
 }
 
 function addBookToLibrary(title, author, pages) {
   // take params, create a book then store it in the array
   const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
+
+  for (i in myLibrary) {
+    // console.log (Book)
+  }
 }
+
 let bookLog = document.getElementById("bookLog");
 bookLog.addEventListener("submit", (event) => {
   event.preventDefault();
-  // let title = document.getElementById("title");
-  // let author = document.getElementById("author");
-  // let pages = document.getElementById("pages");
 
   if (title.value == "" || author.value == "" || pages.value == "") {
     console.log("please fill the form");
   } else {
-    console.log(`${title.value} ${author.value}. ${pages.value}`);
   }
+  addBookToLibrary();
+  console.log(myLibrary[i]);
+  // alert(myLibrary[i].title); 
+
   title.value = "";
   author.value = "";
   pages.value = "";
-  addBookToLibrary();
-console.log(myLibrary);
-
 });
